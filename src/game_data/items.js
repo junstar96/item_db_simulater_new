4421d9c1eecd4a12e75603e911b92e16f5a1b9c2
leejunsung <junstar96@naver.com>
2024-11-28 14:51:40 +0900

    Update items.js

--- src/game_data/items.js
+++ src/game_data/items.js
@@ -4,6 +4,7 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken'
 import UserToken from '../middleware/auth.middlewares.js'
 import { Prisma } from '@prisma/client';
+import logincheck from '../middleware/login_or_not_login.js'
 
 
 const router = express.Router();
@@ -29,9 +30,6 @@ router.post('/items', async (req, res, next) => {
     }
 
     
-    
-
-    const hashedPassword = await bcrypt.hash(password, 10);
     const result = await prisma.$transaction(async (tx) => {
       // Users 테이블에 사용자를 추가합니다.
       const user = await tx.items.create({
@@ -40,24 +38,13 @@ router.post('/items', async (req, res, next) => {
         },
       });
 
-      
-      //throw new Error("고의로 발생한 에러")
-      // UserInfos 테이블에 사용자 정보를 추가합니다.
-      // const userInfo = await tx.userInfos.create({
-      //   data: {
-      //     userId: user.userId, // 생성한 유저의 userId를 바탕으로 사용자 정보를 생성합니다.
-      //     name,
-      //     age,
-      //     gender: gender.toUpperCase(), // 성별을 대문자로 변환합니다.
-      //     profileImage,
-      //   },
-      // });
-
       return [user];
-    },{
-      isolationLevel : Prisma.TransactionIsolationLevel.ReadCommitted
+    }, {
+      isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted
     }
-  )
+    )
+
+    
 
 
 
@@ -73,58 +60,31 @@ router.post('/items', async (req, res, next) => {
 
 
 
-/** 로그인 API **/
-router.post('/sign-in', async (req, res, next) => {
-  const { name, password } = req.body;
-  const user = await prisma.users.findFirst({ where: { name : name } });
+/** 아이템 수정 **/
+router.patch('/items/:itemId', async (req, res, next) => {
+  const { itemId } = req.params;
+  const check_body = req.body;
+  const items = await prisma.items.findFirst({ where: { itemId: +itemId } });
 
-  if (!user)
-    return res.status(401).json({ message: '존재하지 않는 계정입니다.' });
+  if (!items)
+    return res.status(401).json({ message: '존재하지 않는 아이템입니다.' });
   // 입력받은 사용자의 비밀번호와 데이터베이스에 저장된 비밀번호를 비교합니다.
-  else if (!(await bcrypt.compare(password, user.password)))
-    return res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' });
 
-  
-  const token = jwt.sign(
-    {
-      userId: user.userId,
+  const changedItem = await prisma.items.update({
+    data: {
+      ...check_body
     },
-    'custom-secret-key',
-  );
+    where: {
+      itemId: items.itemId
+    }
+  })
 
-  res.cookie('authorization', `Bearer ${token}`);
 
-  
-  return res.status(200).json({ message: '로그인 성공' });
+
+
+  return res.status(201).json({ message: '로그인 성공' });
 });
 
-// src/routes/users.route.js
-
-/** 사용자 조회 API **/
-router.get('/users', UserToken, async (req, res, next) => {
-  console.log(req.body);
-
-  // const user = await prisma.users.findFirst({
-  //   where: { userId: +userId },
-  //   select: {
-  //     userId: true,
-  //     email: true,
-  //     createAt: true,
-  //     updateAt: true,
-  //     userInfos: {
-  //       // 1:1 관계를 맺고있는 UserInfos 테이블을 조회합니다.
-  //       select: {
-  //         name: true,
-  //         age: true,
-  //         gender: true,
-  //         profileImage: true,
-  //       },
-  //     },
-  //   },
-  // });
-
-  return res.status(200).json({ data: "읽기 성공" });
-});
 
 // src/routes/users.router.js
 
